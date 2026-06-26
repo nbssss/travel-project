368b9cfb533af4af59d1011b634f4f54e45e279a
Natalia Borysowska <natalia.borysowska@gmail.com>
2026-06-26 12:01:14 +0200

    Delete temporary rules downgrade

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -18,11 +18,6 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
-    rules: {
-      'react-hooks/set-state-in-effect': 'warn',
-      'react-hooks/refs': 'warn',
-      'react-refresh/only-export-components': 'warn',
-    },
   },
   {
     files: ['**/components/ui/**'],
